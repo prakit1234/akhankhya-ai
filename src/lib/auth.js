aceff34350a09fae2e6e87aadc18e6cd65e94379
prakit1234 <saratgogoi78@gmail.com>
2024-12-07 21:27:02 +0530

    fixing deploy erros

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -1,10 +1,6 @@
+
 import { createClient } from '@supabase/supabase-js';
 
-export const supabase = createClient(
-  import.meta.env.VITE_SUPABASE_URL,
-  import.meta.env.VITE_SUPABASE_ANON_KEY
-);
-
-export const signOut = async () => {
-  await supabase.auth.signOut();
-};
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
+export const supabase = createClient(supabaseUrl, supabaseKey);
